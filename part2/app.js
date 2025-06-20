6f22b13fbd8f1b456d9d2a51590c36fd678a1348
An Phan <tqtthuyan71@gmail.com>
2025-06-20 12:39:22 +0000

    [automatic commit] 2025-06-20 12:39:22 UTC 101.115.224.146

--- part2/app.js
+++ part2/app.js
@@ -23,7 +23,7 @@ app.use(session({
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
-const authRouter = require('/');
+const authRouter = require('./routes/authRoutes');
 
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
