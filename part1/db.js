6857f70dea2f96037952463b5157eb9b8ba2885b
An Phan <tqtthuyan71@gmail.com>
2025-06-20 06:32:00 +0000

    [automatic commit] 2025-06-20 06:32:00 UTC 101.115.224.146

--- part1/db.js
+++ part1/db.js
@@ -1,11 +1,11 @@
-const mysql = require('mysql2');
 
+const mysql = require('mysql2');
 
 const connection = mysql.createPool({
     host: 'localhost',
     user: 'root',
     password: '',
-    multipleStatements: true
+    database: 'DogWalkService'
   }).promise();
 
   // connection.connect((err) => {
