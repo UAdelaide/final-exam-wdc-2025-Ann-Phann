fa8b0cec424c4a75c4d6a022839fb763d61a98fd
An Phan <tqtthuyan71@gmail.com>
2025-06-20 07:29:49 +0000

    [automatic commit] 2025-06-20 07:29:48 UTC 101.115.224.146

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -28,7 +28,10 @@ router.get('/api/walkrequests/open', async(req,res) => {
             SELECT wr.request_id, d.name AS dog_name,
                     wr.requested_time, wr.duration_minutes,
                     wr.location, u.username
-            FROM
+            FROM WalkRequests wr
+            JOIN Dogs d ON wr.dog_id = d.dog_id
+            JOIN Users u ON d.owner_id = u.user_id
+            WHERE wr.status = 'open'
         `);
     } catch (error) {
         console.error('Error in fetching walkrequest open: ', error);
