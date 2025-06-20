1e1074fe0b653a133dca3b9a946b06956ef0bf45
An Phan <tqtthuyan71@gmail.com>
2025-06-20 07:17:44 +0000

    [automatic commit] 2025-06-20 07:17:44 UTC 101.115.224.146

--- part1/app.js
+++ part1/app.js
@@ -4,7 +4,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mysql = require('mysql2/promise');
 
-const api = require('./routes/api');
+const apiRouter = require('./routes/api');
 
 var app = express();
 
