2cc3e19c907baf5b83e5195f854b04882dab791d
An Phan <tqtthuyan71@gmail.com>
2025-06-20 07:42:04 +0000

    [automatic commit] 2025-06-20 07:42:03 UTC 101.115.224.146

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -89,12 +89,16 @@ router.get('/api/walkers/summary', async (req, res) => {
 /**
 [
   {
-    "request_id": 1,
-    "dog_name": "Max",
-    "requested_time": "2025-06-10T08:00:00.000Z",
-    "duration_minutes": 30,
-    "location": "Parklands",
-    "owner_username": "alice123"
+    "walker_username": "bobwalker",
+    "total_ratings": 2,
+    "average_rating": 4.5,
+    "completed_walks": 2
+  },
+  {
+    "walker_username": "newwalker",
+    "total_ratings": 0,
+    "average_rating": null,
+    "completed_walks": 0
   }
 ]
  */
