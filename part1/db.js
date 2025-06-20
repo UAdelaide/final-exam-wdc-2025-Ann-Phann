66295a85c933a56ea9a2f603c97b2ad8f92b3159
An Phan <tqtthuyan71@gmail.com>
2025-06-20 06:30:38 +0000

    [automatic commit] 2025-06-20 06:30:38 UTC 101.115.224.146

--- part1/db.js
+++ part1/db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 
 
 const connection = mysql.createPool({
-    host: '127.0.0.1',
+    host: '127.lo.0.1',
     user: 'root',
     password: '',
     multipleStatements: true
