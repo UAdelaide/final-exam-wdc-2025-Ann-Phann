2ffbeb44cc6783b6a446a62271ef4b29db6101c4
An Phan <tqtthuyan71@gmail.com>
2025-06-20 10:11:52 +0000

    [automatic commit] 2025-06-20 10:11:52 UTC 101.115.224.146

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -85,7 +85,7 @@ router.get('/api/walkrequests/open', async(req,res) => {
 //   }
 // });
 
-app.get('/api/walkers/summary', async (req, res) => {
+router.get('/api/walkers/summary', async (req, res) => {
   try {
     const [summary] = await db.execute(`SELECT user.username AS walker_username,
       COUNT(r.rating_id) AS total_ratings,
