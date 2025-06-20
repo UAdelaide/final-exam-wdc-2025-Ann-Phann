0bc614923ec51744a13466fbed32890c4587c775
An Phan <tqtthuyan71@gmail.com>
2025-06-20 13:01:50 +0000

    [automatic commit] 2025-06-20 13:01:50 UTC 101.115.224.146

--- part2/routes/authRoutes.js
+++ part2/routes/authRoutes.js
@@ -17,8 +17,7 @@ router.get('/login', async (req, res) => {
         `, [username, password]
         );
 
-        if (users.length > 0) {
-            const user = users[0];
+        if (users.length === 0) {
             
         }
 
