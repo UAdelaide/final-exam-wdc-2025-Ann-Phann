5cc252801a2d77490ffefd83f8f95e6188de76f9
An Phan <tqtthuyan71@gmail.com>
2025-06-21 04:33:17 +0000

    [automatic commit] 2025-06-21 04:33:17 UTC ;; communications error to 208.67.222.222#53: timed out 101.115.224.146

---
+++ part2/routes/registeredDogs.js
@@ -0,0 +1,19 @@
+// 1. /api/dogs
+router.get('/api/dogs', async (req, res) => {
+    try {
+        const [rows] = await db.query(`
+            SELECT
+                d.name AS dog_name,
+                d.size,
+                u.username AS owner_username
+            FROM
+                Dogs d
+            INNER JOIN
+                Users u ON d.owner_id = u.user_id;
+        `);
+        res.json(rows);
+    } catch (error) {
+        console.error('Error fetching dogs:', error);
+        res.status(500).json({ error: 'Failed to retrieve dogs.' });
+    }
+});
