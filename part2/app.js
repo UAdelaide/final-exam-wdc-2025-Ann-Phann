f2f6ebad5d20ca1828142b103efecbce9333ff45
An Phan <tqtthuyan71@gmail.com>
2025-06-21 01:20:49 +0000

    [automatic commit] 2025-06-21 01:20:49 UTC 101.115.224.146

--- part2/app.js
+++ part2/app.js
@@ -27,7 +27,7 @@ const authRouter = require('./routes/authRoutes');
 
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
-app.use('/login', authRouter);
+app.use('/auth', authRouter);
 
 // Export the app instead of listening here
 module.exports = app;
