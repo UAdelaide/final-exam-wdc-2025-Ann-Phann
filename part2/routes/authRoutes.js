695dddab6f0f04357b0f9c77e73a68bbb77f99fb
An Phan <tqtthuyan71@gmail.com>
2025-06-21 01:18:25 +0000

    [automatic commit] 2025-06-21 01:18:25 UTC 101.115.224.146

--- part2/routes/authRoutes.js
+++ part2/routes/authRoutes.js
@@ -52,5 +52,12 @@ router.get('/checkLogin', (req, res) => {
 
 });
 
+router.get = (req, res) => {
+  req.session.destroy((err) => {
+    if (err) return res.status(500).json({ error: 'Logout failed' });
+    res.clearCookie('connect.sid');
+    res.status(200).json({ message: 'Logged out' });
+  });
+};
 module.exports = router;
 
