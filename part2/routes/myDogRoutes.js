f30cdccb538901b590cf7fcc5a10650eb9750821
An Phan <tqtthuyan71@gmail.com>
2025-06-21 02:42:28 +0000

    [automatic commit] 2025-06-21 02:42:27 UTC 101.115.224.146

--- part2/routes/myDogRoutes.js
+++ part2/routes/myDogRoutes.js
@@ -3,7 +3,13 @@ const router = express.Router();
 const db = require('../models/db');
 
 router.get('/myDog', async (req, res) => {
-    const 
+    if (!req.session.user) {
+        return res.status(401).json({ error: 'Not logged in' });
+    }
+
+    const ownerId = req.session.user.id;
+
+
     const [dogList] = db.query(`
         SELECT dog_id, name FROM Dogs WHERE owner_id = ?
     `, )
