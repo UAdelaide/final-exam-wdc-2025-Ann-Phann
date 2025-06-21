bd8e51c57cda13e681d957b4bd057a66c98bdab8
An Phan <tqtthuyan71@gmail.com>
2025-06-21 00:58:10 +0000

    [automatic commit] 2025-06-21 00:58:10 UTC 101.115.224.146

--- part2/routes/authRoutes.js
+++ part2/routes/authRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models/db');
 
-router.get('/login', async (req, res) => {
+router.post('/login', async (req, res) => {
     const { username, password } = req.body;
 
     // input validation
